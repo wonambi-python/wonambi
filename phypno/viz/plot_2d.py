6193e0034699920ada669727012cb060230cfdb3
Gio Piantoni <github@gpiantoni.com>
2014-01-13 14:41:54 -0500

    simplify image 2d plotting, but not nearly complete yet

--- phypno/viz/plot_2d.py
+++ phypno/viz/plot_2d.py
@@ -1,12 +1,18 @@
+from numpy import log, isinf, NaN
 from pyqtgraph import ImageView
 
 
-def plot_data(data):
+def plot_data(data, zlog=False):
     """Plot data in 2d.
 
     """
     imv = ImageView()
     imv.show()
-    imv.setImage(data.data[0, :, :])
+    if zlog:
+        plot_data = log(data.data[0, :, :])
+        plot_data[isinf(plot_data)] = NaN
+    else:
+        plot_data = data.data[0, :, :]
 
+    imv.setImage(plot_data)
     return imv  # avoid garbage-collection
