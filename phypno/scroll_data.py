7e8226701d1746096e909d9b37f2c0a3bdcb2dc0
Gio Piantoni <github@gpiantoni.com>
2014-01-15 18:31:15 -0500

    nicer plot, one central widget and smaller widgets idea

--- phypno/scroll_data.py
+++ phypno/scroll_data.py
@@ -1,7 +1,6 @@
 from __future__ import division
-from numpy import diff, squeeze
-from scipy import arange
-from PySide.QtGui import (QMainWindow, QAction, QIcon, QPainterPath,
+from numpy import diff, squeeze, arange
+from PyQt4.QtGui import (QMainWindow, QAction, QIcon, QToolBar, QFileDialog,
                           QGraphicsView, QGraphicsScene)
 from phypno import Dataset
 
@@ -11,69 +10,101 @@ TODO: use ConfigParser
 
 """
 
-info = {
-    'time': 0, # location in time
-    'xscroll': 300, # amount to scroll in time in pixels
-    'ylim': (-100, 100), # size of the datawindow
-    'chandist': 150 # distance between channels
-}
+config = {
+    'time': 0,  # location in time
+    'xscroll': 300,  # amount to scroll in time in pixels
+    'ylim': (-100, 100),  # size of the datawindow
+    }
 
 
-def plotLine(x, y, xpos, ypos, ylim=(-100, 100)):
-    """Plot a line as simple as possible.
 
-    Parameters
-    ----------
-    x : numpy array
-        position on the x axis
-    y : numpy array
-        position on the x axis (actual data values)
-    xpos : tuple
-        min and max x-positions on the graph
-    ypos : tuple
-        min and max y-positions on the graph
-    ylim : tuple, optional
-        min and max y-values for the plot
+IconOpen = QIcon.fromTheme('document-open')
+IconPrev = QIcon.fromTheme('go-previous')
+IconNext = QIcon.fromTheme('go-next')
+IconUp = QIcon.fromTheme('go-up')
+IconDown = QIcon.fromTheme('go-down')
 
-    TODO: xpos and ypos should probably be replaced by view
-    TODO: this should probably be in an independent module
 
-    Returns
-    -------
-    instance of QPainterPath
+class Scroll_Data(QMainWindow):
 
-    """
+    def __init__(self):
+        super().__init__()
 
-    path = QPainterPath()
+        self.info = {}
 
-    left = xpos[0]
-    top = ypos[1]
-    width = xpos[1] - xpos[0]
-    height = ypos[1] - ypos[0]
-    path.addRect(left, top, width, height)
+        ActOpen = QAction(IconOpen, 'Open', self)
+        ActOpen.triggered.connect(self.ActOpen)
 
-    x += left
-    y *= -1 # because QT counts from top, voltage is from bottom
-    yratio = height / diff(ylim) # stretch factor
-    y *= yratio
-    y = y + top + ylim[1] * yratio
+        ActPrev = QAction(IconPrev, 'Previous Page', self)
+        ActPrev.setShortcut('<')
+        ActPrev.triggered.connect(self.ActPrev)
 
-    path.moveTo(xpos[0], top + height / 2)
-    for i_x, i_y in zip(x, y):
-        path.lineTo(i_x, i_y)
+        ActNext = QAction(IconNext, 'Next Page', self)
+        ActNext.setShortcut('>')
+        ActNext.triggered.connect(self.ActNext)
 
-    return path
+        menu = self.addToolBar('File Management')
+        menu.addAction(ActOpen)
 
-def readdat(chan, i):
-    d = Dataset('/home/gio/tools/phypno/test/data/sample.edf')
-    data = d.read_data(chan=['FZ'], begsam=i, endsam=i + 400)
-    return data.data
+        menu = self.addToolBar('Scroll')
+        menu.addAction(ActPrev)
+        menu.addAction(ActNext)
+
+
+
+    def ActOpen(self):
+        self.info['dataset'] = QFileDialog.getOpenFileName(self,
+                                                              'Open file',
+                    '/home/gio/tools/phypno/test/data')
+
+        self.info['d'] = Dataset(self.info['dataset'])
+
+        self.read_data(0)
+        self.plot_data()
+
+    def ActPrev(self):
+        pass
+
+    def ActNext(self):
+
+        self.plot_data()
+
+    def read_data(self, idx):
+        self.info['data'] = self.info['d'].read_data(chan=['PZ'],
+                                                     begsam=idx,
+                                                     endsam=idx +
+                                                     config['xscroll'])
+
+    def plot_data(self):
+        p = PlotWidget()
+        dat, time = self.info['data']()
+        p.plotItem.plot(time, squeeze(dat, axis=0))
+        self.setCentralWidget(p)
+
+
+
+q = Scroll_Data()
+q.show()
+
+
+
+
+
+    def ActUp(self):
+
+        self.info['time'] += self.info['xscroll']
+        self.addChannels()
+
+    def ActDown(self):
+
+        self.info['time'] += self.info['xscroll']
+        self.addChannels()
 
 
 class SleepScoring(QMainWindow):
 
     def __init__(self):
-        super(SleepScoring, self).__init__()
+        super().__init__()
 
         self.info = info
 
@@ -98,55 +129,6 @@ class SleepScoring(QMainWindow):
         self.setWindowTitle('Sleep Scoring')
         self.show()
 
-    def toolbar(self):
-
-        # Icons
-        IconOpen = QIcon.fromTheme('document-open')
-        IconPrev = QIcon.fromTheme('go-previous')
-        IconNext = QIcon.fromTheme('go-next')
-        IconUp = QIcon.fromTheme('go-up')
-        IconDown = QIcon.fromTheme('go-down')
-
-        ActOpen = QAction(IconOpen, 'Open', self)
-        ActOpen.setShortcut('CTRL+o')
-        ActOpen.triggered.connect(self.close)
-
-        ActPrev = QAction(IconPrev, 'Previous Page', self)
-        ActPrev.setShortcut('<')
-        ActPrev.triggered.connect(self.ActPrev)
-
-        ActNext = QAction(IconNext, 'Next Page', self)
-        ActNext.setShortcut('>')
-        ActNext.triggered.connect(self.ActNext)
-
-        toolbar = self.addToolBar('File')
-        toolbar.addAction(ActOpen)
-
-        toolbar = self.addToolBar('Scroll')
-        toolbar.addAction(ActPrev)
-        toolbar.addAction(ActNext)
-
-        return toolbar
-
-    def ActPrev(self):
-
-        self.info['time'] -= self.info['xscroll']
-        self.addChannels()
-
-    def ActNext(self):
-
-        self.info['time'] += self.info['xscroll']
-        self.addChannels()
-
-    def ActUp(self):
-
-        self.info['time'] += self.info['xscroll']
-        self.addChannels()
-
-    def ActDown(self):
-
-        self.info['time'] += self.info['xscroll']
-        self.addChannels()
 
     def addChannels(self):
 
