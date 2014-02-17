ea81258db10ba719383026b17b7529d21c474546
Gio Piantoni <github@gpiantoni.com>
2014-02-17 10:39:17 -0500

    use visvis to plot channels and surface, very neat

--- phypno/viz/plot_3d.py
+++ phypno/viz/plot_3d.py
@@ -5,21 +5,30 @@
 from numpy import hstack, asarray
 import visvis as vv
 
-vv.figure()
-ax = vv.gca()
-ax.axis.visible = False
 
 CHAN_COLOR = (20 / 255., 20 / 255., 20 / 255.)
 SKIN_COLOR = (239 / 255., 208 / 255., 207 / 255.)
 
 
-def plot_surf(surf):
+def _make_fig(fig=None):
+    fig = vv.figure(fig)
+    ax = vv.gca()
+    ax.axis.visible = False
+    return fig
+
+
+def plot_surf(surf, fig=None):
+    fig = _make_fig(fig)
+    ax = vv.gca()
     m = vv.Mesh(ax, vertices=surf.vert, faces=surf.tri)
     m.faceColor = hstack((asarray(SKIN_COLOR), .5))
+    return fig
 
 
-def plot_chan(chan):
-    for i in range(chan.n_chan()):
-        s = vv.solidSphere(list(chan.xyz[i, :]), scaling=2)
-        s.faceColor = (0, 0, 0, 1)
+def plot_chan(chan, fig=None, color=(0, 0, 0, 1)):
+    fig = _make_fig(fig)
+    for one_chan in chan.chan:
+        s = vv.solidSphere(list(one_chan.xyz), scaling=1.5)
+        s.faceColor = color
+    return fig
 
