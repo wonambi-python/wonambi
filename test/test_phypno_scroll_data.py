2c90990b21e25107697860074bc44da21833c744
Gio Piantoni <github@gpiantoni.com>
2014-02-19 09:25:14 -0500

    added skeleton for function to scroll data

---
+++ test/test_phypno_scroll_data.py
@@ -0,0 +1,21 @@
+from inspect import stack
+from logging import getLogger, INFO
+from nose.tools import raises
+from numpy.testing import assert_array_equal
+from subprocess import check_output
+
+
+lg = getLogger('phypno')
+lg.setLevel(INFO)
+git_ver = check_output("git --git-dir=../.git log |  awk 'NR==1' | "
+                       "awk '{print $2}'",
+                       shell=True).decode('utf-8').strip()
+lg.info('phypno ver: ' + git_ver)
+lg.info('Module: ' + __name__)
+
+#-----------------------------------------------------------------------------#
+
+
+def test_scroll_data():
+    lg.info('---\nfunction: ' + stack()[0][3])
+    True
