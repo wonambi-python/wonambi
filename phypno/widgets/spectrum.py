b17f3bd401e7f30d51bec4ef1383510f72d988df
Gio Piantoni <github@gpiantoni.com>
2014-02-24 11:04:14 -0500

    user can adjust limits of spectrum and log, very neat, but still not axis

--- phypno/widgets/spectrum.py
+++ phypno/widgets/spectrum.py
@@ -4,17 +4,46 @@ lg = getLogger(__name__)
 from numpy import log, ceil, floor
 from scipy.signal import welch
 from PySide.QtCore import Qt
-from PySide.QtGui import (QComboBox,
-                          QVBoxLayout,
-                          QWidget,
+from PySide.QtGui import (QCheckBox,
+                          QComboBox,
+                          QFormLayout,
                           QGraphicsView,
                           QGraphicsScene,
+                          QHBoxLayout,
+                          QLineEdit,
                           QPen,
+                          QPushButton,
+                          QVBoxLayout,
+                          QWidget,
                           )
 
 from phypno.widgets.utils import Path
 
 TICK_SIZE = 20
+
+
+def get_text(widget, default):
+    """Get text from widget.
+
+    Parameters
+    ----------
+    widget : instance of widget
+        widget that has the getText method
+    default : float
+        default value for the parameter
+
+    Returns
+    -------
+    float
+        the value in text or default
+
+    """
+    text = widget.text()
+    try:
+        text = float(text)
+    except ValueError:
+        text = default
+    return text
 
 
 class Spectrum(QWidget):
@@ -28,8 +57,20 @@ class Spectrum(QWidget):
         2 values specifying the limit on x-axis
     y_limit : tuple or list
         2 values specifying the limit on y-axis
+    log : bool
+        log-transform the data or not
     idx_chan : instance of QComboBox
         the element with the list of channel names.
+    idx_x_min : instance of QLineEdit
+        value with min x value
+    idx_x_max : instance of QLineEdit
+        value with max x value
+    idx_y_min : instance of QLineEdit
+        value with min y value
+    idx_y_max : instance of QLineEdit
+        value with max y value
+    idx_log : instance of QCheckBox
+        widget that defines if log should be used or not
     idx_fig : instance of QGraphicsView
         the view with the power spectrum
     scene : instance of QGraphicsScene
@@ -43,8 +84,15 @@ class Spectrum(QWidget):
         preferences = self.parent.preferences.values
         self.x_limit = preferences['spectrum/x_limit']
         self.y_limit = preferences['spectrum/y_limit']
+        self.log = True
 
         self.idx_chan = None
+        self.idx_x_min = None
+        self.idx_x_max = None
+        self.idx_y_min = None
+        self.idx_y_max = None
+        self.idx_log = None
+
         self.idx_fig = None
         self.scene = None
 
@@ -55,6 +103,19 @@ class Spectrum(QWidget):
 
         self.idx_chan = QComboBox()
         self.idx_chan.activated.connect(self.display_spectrum)
+
+        self.idx_x_min = QLineEdit(str(self.x_limit[0]))
+        self.idx_x_max = QLineEdit(str(self.x_limit[1]))
+
+        self.idx_y_min = QLineEdit(str(self.y_limit[0]))
+        self.idx_y_max = QLineEdit(str(self.y_limit[1]))
+
+        self.idx_log = QCheckBox('Log-transform')
+        self.idx_log.setCheckState(Qt.Checked)
+
+        apply_button = QPushButton('Apply')
+        apply_button.clicked.connect(self.update_limits)
+
         self.idx_fig = QGraphicsView(self)
         self.idx_fig.scale(1, -1)
 
@@ -63,10 +124,47 @@ class Spectrum(QWidget):
                                     self.y_limit[1] - self.y_limit[0])
         self.idx_fig.setScene(self.scene)
 
+        x_layout = QFormLayout()
+        x_layout.addRow('min x', self.idx_x_min)
+        x_layout.addRow('max x', self.idx_x_max)
+
+        y_layout = QFormLayout()
+        y_layout.addRow('min y', self.idx_y_min)
+        y_layout.addRow('max y', self.idx_y_max)
+
+        button_layout = QVBoxLayout()
+        button_layout.addWidget(self.idx_log)
+        button_layout.addWidget(apply_button)
+
+        control_layout = QHBoxLayout()
+        control_layout.addLayout(x_layout)
+        control_layout.addLayout(y_layout)
+        control_layout.addLayout(button_layout)
+
         layout = QVBoxLayout()
         layout.addWidget(self.idx_chan)
+        layout.addLayout(control_layout)
         layout.addWidget(self.idx_fig)
         self.setLayout(layout)
+
+    def update_limits(self):
+        """Read changes in the way spectrum is plotted."""
+
+        self.x_limit[0] = get_text(self.idx_x_min, self.x_limit[0])
+        self.x_limit[1] = get_text(self.idx_x_max, self.x_limit[1])
+        self.y_limit[0] = get_text(self.idx_y_min, self.y_limit[0])
+        self.y_limit[1] = get_text(self.idx_y_max, self.y_limit[1])
+
+        if self.idx_log.checkState() == Qt.Checked:
+            self.log = True
+        else:
+            self.log = False
+
+        self.scene.setSceneRect(self.x_limit[0], self.y_limit[0],
+                                self.x_limit[1] - self.x_limit[0],
+                                self.y_limit[1] - self.y_limit[0])
+
+        self.display_spectrum()
 
     def update_spectrum(self):
         """Add channel names to the combobox."""
@@ -91,7 +189,13 @@ class Spectrum(QWidget):
         f, Pxx = welch(data, fs=s_freq, nperseg=s_freq)
 
         freq_limit = (self.x_limit[0] <= f) & (f <= self.x_limit[1])
-        self.scene.addPath(Path(f[freq_limit], log(Pxx[freq_limit])))
+
+        if self.log:
+            Pxx_to_plot = log(Pxx[freq_limit])
+        else:
+            Pxx_to_plot = Pxx[freq_limit]
+
+        self.scene.addPath(Path(f[freq_limit], Pxx_to_plot))
 
         self.resizeEvent(None)
 
