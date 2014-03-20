e3a30ef102d25fd884a4f6ac2dfd0e5fc877a915
Gio Piantoni <github@gpiantoni.com>
2014-03-20 13:26:07 -0400

    add stubs for reject

---
+++ phypno/trans/reject.py
@@ -0,0 +1,14 @@
+"""Module to reject bad channels and bad epochs.
+
+"""
+from logging import getLogger
+
+lg = getLogger('phypno')
+
+
+class RejectBadChan:
+    """Class to reject bad channels.
+
+    """
+
+
