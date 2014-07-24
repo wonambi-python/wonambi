83eaf0ac6aadf51ff43c1a46d08fbd053cb6d716
Gio Piantoni <github@gpiantoni.com>
2014-07-24 18:39:29 -0400

    added action to add/delete event types

--- phypno/scroll_data.py
+++ phypno/scroll_data.py
@@ -357,15 +357,13 @@ class MainWindow(QMainWindow):
         answer = QInputDialog.getText(self, 'New Event Type',
                                       'Enter new event\'s name')
         if answer[1]:
-            pass
-            # self.notes.annot.remove_rater(answer[0])
+            self.notes.annot.add_event_type(answer[0])
 
     def action_del_event_type(self):
         answer = QInputDialog.getText(self, 'Delete Event Type',
                                       'Enter event\'s name to delete')
         if answer[1]:
-            pass
-            # self.notes.annot.remove_rater(answer[0])
+            self.notes.annot.remove_event_type(answer[0])
 
     def moveEvent(self, event):
         """Main window is already resized."""
