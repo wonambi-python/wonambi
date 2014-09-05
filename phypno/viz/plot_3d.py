f9c6959f53b484872c01e8a09680229c610278cd
Gio Piantoni <github@gpiantoni.com>
2014-09-05 17:29:09 -0400

    improved documentation for 3d visualization

--- phypno/viz/plot_3d.py
+++ phypno/viz/plot_3d.py
@@ -4,35 +4,46 @@
 from logging import getLogger
 lg = getLogger('phypno')
 
+from functools import partial
 from itertools import chain
 from multiprocessing import Pool
-from os import listdir, remove
+from os import remove
 from os.path import join, splitext
 from subprocess import call
 from tempfile import mkdtemp
-from functools import partial
 
-from numpy import dot, max, mean, min, ones
-from numpy import asarray, NaN, empty, count_nonzero, isnan
+from numpy import (asarray, count_nonzero, dot, empty, isnan, max, mean, min,
+                   NaN, ones)
 from numpy.linalg import norm
-from visvis import Mesh, gca, figure, solidSphere, CM_JET, record
+from visvis import Mesh, gca, figure, solidSphere, CM_JET, screenshot
 
 
 CHAN_COLOR = (20 / 255., 20 / 255., 20 / 255., 1)
 SKIN_COLOR = (239 / 255., 208 / 255., 207 / 255., 0.5)
 
+IMAGE = 'image%09d.jpg'
+
 
 class Viz3:
-    """
+    """The 3d visualization, ordinarily it should hold a surface and electrodes
 
     Attributes
     ----------
+    xyz2surf : ndarray
+        nVertices X nChan matrix, that explains how channel activity should be
+        plotted onto the vertices
+
     _fig : instance of Figure
         current figure
     _ax : instance of Axes
         current axes
+    _surf : instance of phypno.attr.anat.Surf
+        surface to be plotted
+    _h_surf : instance of visvis.wobjects.polygonalModeling.Mesh
+        mesh with the brain surface
+    _h_chan : list of visvis.wobjects.polygonalModeling.OrientableMesh
+        list of objects used to represent channels
     """
-
     def __init__(self):
         self.xyz2surf = None
 
@@ -46,7 +57,35 @@ class Viz3:
 
     def add_surf(self, surf, color=SKIN_COLOR, xyz=None, values=None,
                  limits=None, colormap=CM_JET):
+        """Add surfaces to the visualization.
 
+        Parameters
+        ----------
+        surf : instance of phypno.attr.anat.Surf
+            surface to be plotted
+        color : tuple, optional
+            4-element tuple, representing RGB and alpha, between 0 and 1
+        xyz : ndarray, optional
+            nChan X 3 matrix of the xyz-position of the channels
+        values : ndarray, optional
+            vector with values for each channel
+        limits : tuple of 2 floats, optional
+            min and max values to normalize the color
+        colormap : ndarray, optional
+            matrix for the color coding (such as CM_JET, CM_HOT, ...)
+
+        Notes
+        -----
+        'color' vs 'xyz' and 'values' are mutally exclusive. You need to
+        specify 'xyz' and 'values' if you use those.
+
+        If you specify 'values', then you'll need a matrix called 'xyz2surf'
+        that converts from the channel values to the electrodes.
+        It takes a few seconds to compute but once it's done, plotting is
+        really fast.
+        You can pre-compute it (using arbitrary values) and pass it as
+        attribute to this class.
+        """
         if values is not None and limits is None:
             limits = (min(values), max(values))
 
@@ -75,13 +114,13 @@ class Viz3:
         chan : instance of phypno.attr.Channels
             channels to plot.
         color : tuple, optional
-            4-element tuple, representing RGB and alpha.
+            4-element tuple, representing RGB and alpha, between 0 and 1
         values : ndarray, optional
             vector with values for each electrode
-        limits : 2 float values
-            min and max values
+        limits : tuple of 2 floats, optional
+            min and max values to normalize the color
         colormap : ndarray, optional
-            2d matrix (for example, from visvis import CM_JET)
+            matrix for the color coding (such as CM_JET, CM_HOT, ...)
         """
         # larger if colors are meaningful
         if values is not None:
@@ -105,20 +144,18 @@ class Viz3:
             self._h_chan.append(s)
 
     def update_surf(self, color=None, values=None, limits=None, colormap=None):
-        """Plot values onto the brain surface.
+        """Change values of the brain surface.
 
         Parameters
         ----------
-        values : numpy.ndarray
-            1-d vector with values at each point.
-
-        Notes
-        -----
-        The transformation matrix does the important part of converting the values
-        at each electrode into the values onto the surface. You can specify it as
-        you want, as long as its dimensions are number of vertices X number of
-        values.
-
+        color : tuple, optional
+            4-element tuple, representing RGB and alpha, between 0 and 1
+        values : ndarray, optional
+            vector with values for each channel
+        limits : tuple of 2 floats, optional
+            min and max values to normalize the color
+        colormap : ndarray, optional
+            matrix for the color coding (such as CM_JET, CM_HOT, ...)
         """
         m = self._h_surf
         if values is not None:
@@ -134,7 +171,19 @@ class Viz3:
             m.faceColor = color
 
     def update_chan(self, color=None, values=None, limits=None, colormap=None):
+        """Change values of the electrodes.
 
+        Parameters
+        ----------
+        color : tuple, optional
+            4-element tuple, representing RGB and alpha, between 0 and 1
+        values : ndarray, optional
+            vector with values for each channel
+        limits : tuple of 2 floats, optional
+            min and max values to normalize the color
+        colormap : ndarray, optional
+            matrix for the color coding (such as CM_JET, CM_HOT, ...)
+        """
         for i, s in enumerate(self._h_chan):
 
             if values is not None:
@@ -150,16 +199,27 @@ class Viz3:
                 s.faceColor = color
 
     def animate_surf(self, all_values, output_file):
-        """I cannot modify frame rate in avconv, default is 24"""
+        """Create a movie with the changing values onto the surface.
 
+        Parameters
+        ----------
+        all_values : ndarray
+            nTime X nChan matrix, it will loop over time
+        output_file : str
+            path to file, it can end in '.gif' or in any format used by avconv
+
+        Notes
+        -----
+        It needs 'avconv' to be installed.
+        I cannot modify frame rate in avconv, default is 24. Lots of options
+        but I don't know how to use them.
+        """
         img_dir = mkdtemp()
 
-        rec = record(self._ax)
-        for values in all_values:
+        for i, values in enumerate(all_values):
             self.update_surf(values=values)
             self._fig.DrawNow()
-        rec.Stop()
-        rec.Export(join(img_dir, 'image.png'))
+            screenshot(join(img_dir, IMAGE % i), self._fig, sf=1)
 
         if splitext(output_file)[1] == '.gif':
             _make_gif(img_dir, output_file)
@@ -167,15 +227,27 @@ class Viz3:
             _make_mp4(img_dir, output_file)
 
     def animate_chan(self, all_values, output_file):
+        """Create a movie with the changing values of the electrodes.
 
+        Parameters
+        ----------
+        all_values : ndarray
+            nTime X nChan matrix, it will loop over time
+        output_file : str
+            path to file, it can end in '.gif' or in any format used by avconv
+
+        Notes
+        -----
+        It needs 'avconv' to be installed.
+        I cannot modify frame rate in avconv, default is 24. Lots of options
+        but I don't know how to use them.
+        """
         img_dir = mkdtemp()
 
-        rec = record(self._ax)
-        for values in all_values:
+        for i, values in enumerate(all_values):
             self.update_chan(values=values)
             self._fig.DrawNow()
-        rec.Stop()
-        rec.Export(join(img_dir, 'image.png'))
+            screenshot(join(img_dir, IMAGE % i), self._fig, sf=1)
 
         if splitext(output_file)[1] == '.gif':
             _make_gif(img_dir, output_file)
@@ -187,22 +259,14 @@ class Viz3:
 
         Parameters
         ----------
-        fig : instance of visvis.Figure
-            figure being plotted.
+        output_file : str
+            path to file, it can end in '.gif' or in any format used by avconv
         loop : str, optional
-            'full' (complete rotation) or 'patrol' (half rotation)
+            'full' (complete rotation) or 'hemi' (half rotation) or
+            'consistent' (but both hemispheres have the same direction) or
+            'patrol' (half rotation back and forth)
         step : int, optional
             distance in degrees between frames
-        focus : str, optional
-            'fig' or 'axis', which part of the image should be saved
-        poster : bool, optional
-            make a poster image before the video (it creates "poster.png")
-
-        Returns
-        -------
-        path to dir
-            temporary directory with images
-
         """
         img_dir = mkdtemp()
 
@@ -228,13 +292,10 @@ class Viz3:
             elif loop == 'consistent':
                 angles = range(0, -180, -step)
 
-        rec = record(self._ax)
-        for i in angles:
-            self._ax.camera.azimuth = i
+        for i, ang in enumerate(angles):
+            self._ax.camera.azimuth = ang
             self._fig.DrawNow()
-
-        rec.Stop()
-        rec.Export(join(img_dir, 'image.png'))
+            screenshot(join(img_dir, IMAGE % i), self._fig, sf=1)
 
         if splitext(output_file)[1] == '.gif':
             _make_gif(img_dir, output_file)
@@ -242,6 +303,7 @@ class Viz3:
             _make_mp4(img_dir, output_file)
 
     def center_surf(self):
+        """Center the figure by looking at the side of the hemisphere."""
         center_surf = tuple(mean(self._surf.vert, axis=0))
         self._ax.camera.loc = center_surf
         self._ax.camera.elevation = 0
@@ -252,9 +314,6 @@ class Viz3:
         else:
             self._ax.camera.azimuth = 270
 
-    def draw(self):
-        pass
-
 
 def calc_xyz2surf(surf, xyz, threshold=10):
     """Calculate transformation matrix from xyz values to vertices.
@@ -264,9 +323,9 @@ def calc_xyz2surf(surf, xyz, threshold=10):
     surf : instance of phypno.attr.Surf
         the surface of only one hemisphere.
     xyz : numpy.ndarray
-        N x 3 matrix, with the locations in x, y, z.
-    dist_func : function
-        function used to calculate the distance.
+        nChan x 3 matrix, with the locations in x, y, z.
+    threshold : float
+        distance in mm for a vertex to pick up electrode activity
 
     Returns
     -------
@@ -275,18 +334,16 @@ def calc_xyz2surf(surf, xyz, threshold=10):
 
     Notes
     -----
-    This function is a helper to plot_values_on_surf, by creating a
+    This function is a helper when plotting onto brain surface, by creating a
     transformation matrix from the values in space (f.e. at each electrode) to
     the position of the vertices (used to show the brain surface).
 
-    There are many ways to move from values to vertices. You can either create
-    your own matrix (and skip calculate_chan2surf_trans altogether) or you can
-    pass a function to calculate_chan2surf_trans. The "dist_func" takes two
-    parameters: the first is the xyz position of one vertex, the second is the
-    xyz position of one electrode (i.e. a row of the parameter "xyz").
+    There are many ways to move from values to vertices.
+    You can also create your own matrix (and skip calc_xyz2surf altogether) and
+    pass it as attribute to the main figure.
     Because it's a loop over all the vertices, this function is pretty slow,
     but if you calculate it once, you can reuse it.
-
+    We take advantage of multiprocessing, which speeds it up considerably.
     """
     with Pool() as p:
         xyz2surf = p.map(partial(calc_one_vert, xyz=xyz, thres=threshold),
@@ -299,6 +356,22 @@ def calc_xyz2surf(surf, xyz, threshold=10):
 
 
 def calc_one_vert(one_vert, xyz=None, thres=10):
+    """Calculate how many electrodes influence one vertex.
+
+    Parameters
+    ----------
+    one_vert : ndarray
+        vector of xyz position of a vertex
+    xyz : ndarray
+        nChan X 3 with the position of all the channels
+    threshold : float
+        distance in mm for a vertex to pick up electrode activity
+
+    Returns
+    -------
+    ndarray
+        one vector with values for one vertex
+    """
     trans = empty(xyz.shape[0])
     for i, one_xyz in enumerate(xyz):
         if norm(one_vert - one_xyz) <= thres:
@@ -328,22 +401,16 @@ def _make_gif(img_dir, gif_file):
 
     Parameters
     ----------
-    fig : instance of visvis.Figure
-        figure being plotted.
+    img_dir : path to dir
+        directory with all the imags
     gif_file : path to file
         file where you want to save the gif
-    loop : str, optional
-        'full' (complete rotation) or 'patrol' (half rotation)
-    step : int
-        distance in degrees between frames
-    focus : str
-        'fig' or 'axis', which part of the image should be saved
 
     Notes
     -----
     It requires ''convert'' from Imagemagick
     """
-    call('convert ' + join(img_dir, 'image*.png') + ' ' + gif_file,
+    call('convert ' + join(img_dir, 'image*.jpg') + ' ' + gif_file,
          shell=True)
 
 
@@ -352,41 +419,19 @@ def _make_mp4(img_dir, movie_file):
 
     Parameters
     ----------
-    fig : instance of visvis.Figure
-        figure being plotted.
+    img_dir : path to dir
+        directory with all the imags
     movie_file : path to file
         file where you want to save the movie
-    loop : str, optional
-        'full' (complete rotation) or 'hemi' (half rotation) or 'patrol' (half
-        rotation in two directions) or 'consistent' (like hemi, but in the same
-        direction for both hemispheres)
-    step : int
-        distance in degrees between frames
-    focus : str
-        'fig' or 'axis', which part of the image should be saved
-    poster : bool, optional
-        make a poster image before the video (it creates "poster.png")
 
     Notes
     -----
     It requires ''avconv'' from LibAv
-
     """
-    # name in the images depends on the number of images
-    # I took this part from visvis directly
-    N = len(listdir(img_dir))
-    formatter = '%04d'
-    if N < 10:
-        formatter = '%d'
-    elif N < 100:
-        formatter = '%02d'
-    elif N < 1000:
-        formatter = '%03d'
-
     try:
         remove(movie_file)
     except FileNotFoundError:
         pass
 
-    call('avconv -i ' + join(img_dir, 'image' + formatter + '.png') +
+    call('avconv -i ' + join(img_dir, IMAGE) +
          ' -tune animation ' + movie_file, shell=True)
