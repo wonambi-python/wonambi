ad08b97640141e6b3618a10bbb03188a02c356df
Gio Piantoni <github@gpiantoni.com>
2014-10-23 14:53:48 -0400

    implement a complete wavelet analysis, pretty good

--- phypno/trans/frequency.py
+++ phypno/trans/frequency.py
@@ -1,11 +1,14 @@
 """Module to compute frequency representation.
 
 """
-from logging import getLogger
+from logging import getLogger, DEBUG
 lg = getLogger('phypno')
 
-from numpy import arange, empty, where
-from scipy.signal import welch, morlet, fftconvolve
+from copy import deepcopy
+
+from numpy import arange, empty, exp, max, mean, pi, real, sqrt, where
+from numpy.linalg import norm
+from scipy.signal import welch, fftconvolve
 
 from ..datatype import ChanFreq, ChanTimeFreq
 
@@ -18,6 +21,9 @@ class Freq:
     method : str
         the method to compute the power spectrum, such as 'welch'
 
+    Notes
+    -----
+    TODO: check that power does not change if duration becomes longer
     """
     def __init__(self, method='welch', duration=2, **options):
         implemented_methods = ('welch', )
@@ -98,10 +104,24 @@ class TimeFreq:
     For method 'morlet', the following options should be specified:
         foi : ndarray or list or tuple
             vector with frequency of interest
-        M_in_s : int
-            duration of the wavelet in seconds
-        w : int
-            Omega0
+        ratio : float
+            ratio for a wavelet family ( = freq / sigma_f)
+        sigma_f : float
+            standard deviation of the wavelet in frequency domain
+        dur_in_sd : float
+            duration of the wavelet, given as number of the standard deviation
+            in the time domain, in one side.
+        dur_in_s : float
+            total duration of the wavelet, two-sided (i.e. from start to
+            finish)
+        normalization : str
+            'area' means that energy is normalized to 1, 'peak' means that the
+            peak is set at 1, 'max' is a normalization used by nitime that I
+            don't understand.
+        zero_mean : bool
+            make sure that the wavelet has zero mean (only relevant if ratio
+            < 5)
+
     For method 'welch', the following options should be specified:
         duraton : int
             duration of the window to compute the power spectrum, in s
@@ -119,8 +139,12 @@ class TimeFreq:
 
         if method == 'morlet':
             default_options = {'foi': None,
-                               'M_in_s': 1,
-                               'w': 5,
+                               'ratio': 5,
+                               'sigma_f': None,
+                               'dur_in_sd': 4,
+                               'dur_in_s': None,
+                               'normalization': 'area',
+                               'zero_mean': False,
                                }
         elif method == 'welch':
             default_options = {'duration': 1,
@@ -129,9 +153,6 @@ class TimeFreq:
 
         self.method = method
         default_options.update(options)
-        for name, value in default_options.items():
-            if value is None:
-                raise ValueError('Specify a value for ' + name)
         self.options = default_options
 
     def __call__(self, data):
@@ -172,11 +193,13 @@ class TimeFreq:
         timefreq.data = empty(data.number_of('trial'), dtype='O')
 
         if self.method == 'morlet':
-            wavelets = _create_morlet(self.options, data.s_freq)
+
+            wavelets = _create_morlet(deepcopy(self.options), data.s_freq)
 
             for i in range(data.number_of('trial')):
                 lg.info('Processing trial # {0: 6}'.format(i))
                 timefreq.axis['freq'][i] = self.options['foi']
+                timefreq.axis['time'][i] = data.axis['time'][i]
 
                 timefreq.data[i] = empty((data.number_of('chan')[i],
                                           data.number_of('time')[i],
@@ -184,8 +207,8 @@ class TimeFreq:
                                          dtype='complex')
                 for i_c, chan in enumerate(data.axis['chan'][i]):
                     dat = data(trial=i, chan=chan)
-                    for i_f, f in enumerate(self.options['foi']):
-                        tf = fftconvolve(dat, wavelets[i_f, :], 'same')
+                    for i_f, wavelet in enumerate(wavelets):
+                        tf = fftconvolve(dat, wavelet, 'same')
                         timefreq.data[i][i_c, :, i_f] = tf
 
         elif self.method == 'welch':
@@ -246,39 +269,92 @@ def _create_morlet(options, s_freq):
     ndarray
         nFreq X nSamples matrix containing the complex Morlet wavelets.
 
-    Notes
-    -----
-    Wavelets are not scaled by the frequency.
-
     """
-    M = options['M_in_s'] * s_freq
-
-    wavelets = empty((len(options['foi']), M), dtype='complex')
-    for i, f in enumerate(options['foi']):
-        scaling = _compute_scaling(f, M, options['w'], s_freq)
-        wavelets[i, :] = morlet(M, options['w'], scaling)
+    wavelets = []
+    foi = options.pop('foi')
+    for f in foi:
+        wavelets.append(morlet(f, s_freq, **options))
 
     return wavelets
 
 
-def _compute_scaling(f, M, w, r):
-    """Compute the scaling factor for a specific frequency.
+def morlet(freq, s_freq, ratio=5, sigma_f=None, dur_in_sd=4, dur_in_s=None,
+           normalization='area', zero_mean=False):
+    """Create a Morlet wavelet.
 
     Parameters
     ----------
-    f : int or float
-        frequency of interest
-    M : int
-        duration of the wavelet in samples
-    w : int
-        Omega0 (wavelet parameter)
-    r : int or float
-        sampling frequency of the data
+    freq : float
+        central frequency of the wavelet
+    s_freq : int
+        sampling frequency
+    ratio : float
+        ratio for a wavelet family ( = freq / sigma_f)
+    sigma_f : float
+        standard deviation of the wavelet in frequency domain
+    dur_in_sd : float
+        duration of the wavelet, given as number of the standard deviation in
+        the time domain, in one side.
+    dur_in_s : float
+        total duration of the wavelet, two-sided (i.e. from start to finish)
+    normalization : str
+        'area' means that energy is normalized to 1, 'peak' means that the peak
+        is set at 1, 'max' is a normalization used by nitime that I don't
+        understand.
+    zero_mean : bool
+        make sure that the wavelet has zero mean (only relevant if ratio < 5)
 
     Returns
     -------
-    float
-        scaling factor
+    ndarray
+        vector containing the complex Morlet wavelets
+
+    Notes
+    -----
+    'ratio' and 'sigma_f' are mutually exclusive. If you use 'sigma_f', the
+    standard deviation stays the same for all the frequency. It's more common
+    to specify a constant ratio for the wavelet family, so that the frequency
+    resolution changes with the frequency of interest.
+
+    'dur_in_sd' and 'dur_in_s' are mutually exclusive. 'dur_in_s' specifies the
+    total duration (from start to finish) of the window. 'dur_in_sd' calculates
+    the total duration as the length in standard deviations in the time domain:
+    dur_in_s = dur_in_sd * 2 * sigma_t, with sigma_t = 1 / (2 * pi * sigma_f)
 
     """
-    return (M * f) / (2 * w * r)
+    if sigma_f is None:
+        sigma_f = freq / ratio
+    else:
+        ratio = freq / sigma_f
+    sigma_t = 1 / (2 * pi * sigma_f)
+
+    if ratio < 5 and not zero_mean:
+        lg.info('The wavelet won''t have zero mean, set zero_mean=True')
+
+    if dur_in_s is None:
+        dur_in_s = sigma_t * dur_in_sd * 2
+
+    dur_in_s /= 2
+    t = arange(-dur_in_s, dur_in_s, 1 / s_freq)
+
+    w = exp(1j * 2 * pi * freq * t)
+    if zero_mean:
+        w -= exp(-1 / 2 * ratio ** 2)
+
+    w *= exp(-t ** 2 / (2 * sigma_t ** 2))
+
+    if normalization == 'area':
+        w /= sqrt(sqrt(pi) * sigma_t * s_freq)
+    elif normalization == 'max':
+        w /= 2 * sigma_t * sqrt(2 * pi) / s_freq
+    elif normalization == 'peak':
+        pass
+
+    if lg.level == DEBUG:
+        lg.debug('At freq {0:.2f}Hz, sigma_f={1:.2f}Hz, sigma_t={2:.3f}s, '
+                 'total duration={3:.3f}s'.format(freq, sigma_f, sigma_t,
+                                                  dur_in_s))
+        lg.debug('    Real peak={0:.3f}, Mean={1:.6f}, '
+                 'Energy={2:.3f}'.format(max(real(w)), mean(w), norm(w) ** 2))
+
+    return w
