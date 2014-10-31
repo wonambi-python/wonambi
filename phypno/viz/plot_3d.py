575ec12c59d7a8aef64626686d9538f23f149199
Gio Piantoni <github@gpiantoni.com>
2014-10-31 14:00:32 -0400

    improve functions to project onto vertex. Get rid of binary, but use gaussian or inverse function

--- phypno/viz/plot_3d.py
+++ phypno/viz/plot_3d.py
@@ -12,10 +12,12 @@ from os.path import join, splitext
 from subprocess import call
 from tempfile import mkdtemp
 
-from numpy import (asarray, count_nonzero, dot, empty, isnan, max, mean, min,
-                   NaN, ones)
+from numpy import (asarray, atleast_2d, dot, empty, exp, isnan, max, mean, min,
+                   NaN, ones, nansum)
 from numpy.linalg import norm
 from visvis import Mesh, gca, figure, solidSphere, CM_JET, screenshot
+
+gauss = lambda x, s: exp(-.5 * (x ** 2 / s ** 2))
 
 
 CHAN_COLOR = (20 / 255., 20 / 255., 20 / 255., 1)
@@ -315,7 +317,7 @@ class Viz3:
             self._ax.camera.azimuth = 270
 
 
-def calc_xyz2surf(surf, xyz, threshold=10):
+def calc_xyz2surf(surf, xyz, threshold=20, exponent=None, std=None):
     """Calculate transformation matrix from xyz values to vertices.
 
     Parameters
@@ -324,8 +326,13 @@ def calc_xyz2surf(surf, xyz, threshold=10):
         the surface of only one hemisphere.
     xyz : numpy.ndarray
         nChan x 3 matrix, with the locations in x, y, z.
+    std : float
+        distance in mm of the Gaussian kernel
+    exponent : int
+        inverse law (1-> direct inverse, 2-> inverse square, 3-> inverse cube)
     threshold : float
-        distance in mm for a vertex to pick up electrode activity
+        distance in mm for a vertex to pick up electrode activity (if distance
+        is above the threshold, one electrode does not affect a vertex).
 
     Returns
     -------
@@ -338,25 +345,54 @@ def calc_xyz2surf(surf, xyz, threshold=10):
     transformation matrix from the values in space (f.e. at each electrode) to
     the position of the vertices (used to show the brain surface).
 
-    There are many ways to move from values to vertices.
+    There are many ways to move from values to vertices. The crucial parameter
+    is the function at which activity decreases in respect to the distance. You
+    can have an inverse relationship by specifying 'exponent'. If 'exponent' is
+    2, then the activity will decrease as inverse square of the distance. The
+    function can be a Gaussian. With std, you specify the width of the gaussian
+    kernel in mm.
+    For each vertex, it uses a threshold based on the distance ('threshold'
+    value, in mm). Finally, it normalizes the contribution of all the channels
+    to 1, so that the sum of the coefficients for each vertex is 1.
+
     You can also create your own matrix (and skip calc_xyz2surf altogether) and
     pass it as attribute to the main figure.
     Because it's a loop over all the vertices, this function is pretty slow,
     but if you calculate it once, you can reuse it.
     We take advantage of multiprocessing, which speeds it up considerably.
     """
+    if exponent is None and std is None:
+        exponent = 1
+
+    if exponent is not None:
+        lg.debug('Vertex values based on inverse-law, with exponent ' +
+                 str(exponent))
+        funct = partial(calc_one_vert_inverse, xyz=xyz, exponent=exponent)
+    elif std is not None:
+        lg.debug('Vertex values based on gaussian, with s.d. ' + str(std))
+        funct = partial(calc_one_vert_gauss, xyz=xyz, std=std)
+
     with Pool() as p:
-        xyz2surf = p.map(partial(calc_one_vert, xyz=xyz, thres=threshold),
-                         surf.vert)
+        xyz2surf = p.map(funct, surf.vert)
 
     xyz2surf = asarray(xyz2surf)
+
+    if exponent is not None:
+        threshold_value = (1 / (threshold ** exponent))
+    elif std is not None:
+        threshold_value = gauss(threshold, std)
+    lg.debug('Values thresholded at ' + str(threshold_value))
+
+    xyz2surf[xyz2surf < threshold_value] = NaN
+    xyz2surf /= atleast_2d(nansum(xyz2surf, axis=1)).T
     xyz2surf[isnan(xyz2surf)] = 0
 
     return xyz2surf
 
 
-def calc_one_vert(one_vert, xyz=None, thres=10):
-    """Calculate how many electrodes influence one vertex.
+def calc_one_vert_inverse(one_vert, xyz=None, exponent=None):
+    """Calculate how many electrodes influence one vertex, using the inverse
+    function.
 
     Parameters
     ----------
@@ -364,8 +400,8 @@ def calc_one_vert(one_vert, xyz=None, thres=10):
         vector of xyz position of a vertex
     xyz : ndarray
         nChan X 3 with the position of all the channels
-    threshold : float
-        distance in mm for a vertex to pick up electrode activity
+    exponent : int
+        inverse law (1-> direct inverse, 2-> inverse square, 3-> inverse cube)
 
     Returns
     -------
@@ -374,11 +410,32 @@ def calc_one_vert(one_vert, xyz=None, thres=10):
     """
     trans = empty(xyz.shape[0])
     for i, one_xyz in enumerate(xyz):
-        if norm(one_vert - one_xyz) <= thres:
-            trans[i] = 1
-        else:
-            trans[i] = NaN
-    return trans / count_nonzero(~isnan(trans))
+        trans[i] = 1 / (norm(one_vert - one_xyz) ** exponent)
+    return trans
+
+
+def calc_one_vert_gauss(one_vert, xyz=None, std=None):
+    """Calculate how many electrodes influence one vertex, using a Gaussian
+    function.
+
+    Parameters
+    ----------
+    one_vert : ndarray
+        vector of xyz position of a vertex
+    xyz : ndarray
+        nChan X 3 with the position of all the channels
+    std : float
+        distance in mm of the Gaussian kernel
+
+    Returns
+    -------
+    ndarray
+        one vector with values for one vertex
+    """
+    trans = empty(xyz.shape[0])
+    for i, one_xyz in enumerate(xyz):
+        trans[i] = gauss(norm(one_vert - one_xyz), std)
+    return trans
 
 
 def _set_value_to_chan(s, v):
