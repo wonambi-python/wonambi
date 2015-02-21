13eee869fc729ddefb52714834d25d5c21748aec
Gio Piantoni <gio@gpiantoni.com>
2015-02-20 16:26:29 -0800

    bipolar montage should work fine

--- phypno/trans/montage.py
+++ phypno/trans/montage.py
@@ -3,7 +3,10 @@ lg = getLogger('phypno')
 
 from copy import deepcopy
 
-from numpy import mean
+from numpy import asarray, dot, zeros, where, c_, mean
+from numpy.linalg import norm
+
+from ..attr import Channels
 
 
 class Montage:
@@ -35,7 +38,9 @@ class Montage:
             if (not isinstance(ref_chan, (list, tuple)) or
                 not all(isinstance(x, str) for x in ref_chan)):
                     raise TypeError('chan should be a list of strings')
-                    
+
+        if ref_chan is None:
+            ref_chan = []  # TODO: check bool for ref_chan
         self.ref_chan = ref_chan
         self.ref_to_avg = ref_to_avg
         self.bipolar = bipolar
@@ -57,27 +62,89 @@ class Montage:
         if self.bipolar:
             if not data.attr['chan']:
                 raise ValueError('Data should have Chan information in attr')
-            
-all_chan = Channels('/home/gio/recordings/EM09/doc/elec/EM09_elec_pos-names_sessA.csv')
-chan_list = _select_channels('/home/gio/recordings/EM09/doc/elec/EM09_eeg_xltek_sessA_d05_11_53_38_channels.json', 'grid')
 
-chan = all_chan(lambda x: x.label in chan_list)
-        
-        
+            _assert_equal_channels(data.axis['chan'])
+            chan_in_data = data.axis['chan'][0]
+            chan, trans = _create_bipolar_chan(data.attr['chan'],
+                                               chan_in_data, self.bipolar)
+            data.attr['chan'] = chan
+
         mdata = deepcopy(data)
 
-        if self.ref_to_avg or len(self.ref_chan) > 0:
+        if self.ref_to_avg or not self.ref_chan:
 
             for i in range(mdata.number_of('trial')):
-                if self.ref_to_avg:
-                    self.ref_chan = data.axis['chan'][0]
+                if self.ref_to_avg or len(self.ref_chan) > 0:  # TODO: check bool for ref_chan
+                    if self.ref_to_avg:
+                        self.ref_chan = data.axis['chan'][0]
 
-                ref_data = mdata(trial=i, chan=self.ref_chan)
-                mdata.data[i] = (mdata(trial=i) -
-                                 mean(ref_data,
-                                      axis=mdata.index_of('chan')))
+                    ref_data = mdata(trial=i, chan=self.ref_chan)
+                    mdata.data[i] = (mdata(trial=i) -
+                                     mean(ref_data,
+                                          axis=mdata.index_of('chan')))
+
+                elif self.bipolar:
+
+                    if not data.index_of('chan') == 0:
+                        raise ValueError('For matrix multiplication to work, '
+                                         'the first dimension should be chan')
+                    mdata.data[i] = dot(trans, mdata(trial=i))
+                    mdata.axis['chan'][i] = asarray(chan.return_label(),
+                                                    dtype='U')
 
         return mdata
 
 
+def _assert_equal_channels(axis):
+    """check that all the trials have the same channels, in the same order.
 
+    Parameters
+    ----------
+    axis : ndarray of ndarray
+        one of the data axis
+
+    Raises
+    ------
+
+    """
+    for i0 in axis:
+        for i1 in axis:
+            if not all(i0 == i1):
+                raise ValueError('The channels for all the trials should have '
+                                 'the same labels, in the same order.')
+
+
+def _create_bipolar_chan(chan, chan_in_data, max_dist):
+    chan = chan(lambda x: x.label in chan_in_data)
+
+    chan_dist = zeros((chan.n_chan, chan.n_chan), dtype='bool')
+    for i0, chan0 in enumerate(chan.chan):
+        for i1, chan1 in enumerate(chan.chan):
+            if i0 < i1 and norm(chan0.xyz - chan1.xyz) < max_dist:
+                chan_dist[i0, i1] = True
+
+    x_all, y_all = where(chan_dist)
+
+    bipolar_labels = []
+    bipolar_xyz = []
+    bipolar_trans = []
+
+    for x0, x1 in zip(x_all, y_all):
+
+        new_label = chan.chan[x0].label + '-' + chan.chan[x1].label
+        bipolar_labels.append(new_label)
+
+        xyz = mean(c_[chan.chan[x0].xyz, chan.chan[x1].xyz], axis=1)
+        bipolar_xyz.append(xyz)
+
+        trans = zeros(chan.n_chan)
+        trans[x0] = 1
+        trans[x1] = -1
+        bipolar_trans.append(trans)
+
+    bipolar_xyz = c_[bipolar_xyz]
+    bipolar_trans = c_[bipolar_trans]
+
+    bipolar = Channels(bipolar_labels, bipolar_xyz)
+
+    return bipolar, bipolar_trans
