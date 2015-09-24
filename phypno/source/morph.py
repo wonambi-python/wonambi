0b5ea3647404027b05d620c1baa30bf13c1e7312
Gio Piantoni <github@gpiantoni.com>
2015-09-24 09:16:12 -0400

    morph pass smooth as option

--- phypno/source/morph.py
+++ phypno/source/morph.py
@@ -6,9 +6,10 @@ from mne import morph_data, SourceEstimate
 
 
 class Morph:
-    def __init__(self, from_surf, to_surf='fsaverage'):
+    def __init__(self, from_surf, to_surf='fsaverage', smooth=None):
         self.from_surf = from_surf
         self.to_surf = to_surf
+        self.smooth = smooth
 
     def __call__(self, data):
 
@@ -25,8 +26,8 @@ class Morph:
         stc = SourceEstimate(atleast_2d(data.data[0]).T, vertices=vertices,
                              tstep=0, tmin=0)
         m = morph_data(from_surf_name, self.to_surf, stc,
-                       subjects_dir=SUBJECTS_DIR, grade=None, smooth=None,
-                       verbose=False)
+                       subjects_dir=SUBJECTS_DIR, grade=None,
+                       smooth=self.smooth, verbose=False)
 
         output.data[0] = squeeze(m.data, axis=1)
         output.axis['surf'][0] = arange(m.data.shape[0])
