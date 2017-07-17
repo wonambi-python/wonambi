a22dbb5b11748900f3bd684a6fe220ee3cd5f612
Gio at UMCU <github@gpiantoni.com>
2017-07-17 11:53:33 +0200

    simple examples from sphinx-gallery

---
+++ examples/plot_code.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+A short Python script
+=====================
+
+A script that is not executed when gallery is generated but nevertheless
+gets included as an example.
+Doing a list
+"""
+
+# test gio
+
+print([i for i in range(10)])
---
+++ examples/plot_strings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Constrained Text output frame
+=============================
+
+This example captures the stdout and includes it in the
+example. If output is too long it becomes automatically
+framed into a text area.
+
+"""
+
+# Code source: Óscar Nájera
+# License: BSD 3 clause
+
+print('This is a long test Output\n' * 50)
+
+####################################
+# One line out
+
+print('one line out')
