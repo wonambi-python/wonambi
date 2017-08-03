cb9b71060c3f3898eb1530d2996f7e9ebd4996d5
Gio at UMCU <github@gpiantoni.com>
2017-08-03 15:06:43 +0200

    good coverage of anat

--- tests/test_phypno_attr_anat.py
+++ tests/test_phypno_attr_anat.py
@@ -21,7 +21,7 @@ def test_import_freesurfer_LUT_01():
     idx, label, rgba = import_freesurfer_LUT()
     assert  idx[-1] == 14175
     assert label[-1] == 'wm_rh_S_temporal_transverse'
-    assert_array_equal(rgba[-1], array([ 221.,   60.,   60.,    0.]))
+    assert_array_equal(rgba[-1, :], array([ 221.,   60.,   60.,    0.]))
 
 
 def test_import_freesurfer_LUT_02():
@@ -53,43 +53,37 @@ def test_Surf_02():
 
 def test_Freesurfer_01():
     with raises(OSError):
-        Freesurfer('')
+        Freesurfer('does_not_exist')
 
 
-"""
+fs = Freesurfer(fs_path)
 
-fs = Freesurfer(fs_dir)
-
+def test_Freesurfer_02():
+    fs.read_brain()
 
 def test_Freesurfer_03():
-    assert fs.dir == fs_dir
+    assert fs.dir == fs_path
     assert fs.lookuptable['index'][-1] == 14175
     assert fs.lookuptable['label'][-1] == 'wm_rh_S_temporal_transverse'
-    assert all(fs.lookuptable['RGBA'][-1, :] == array([221., 60., 60., 0]))
+    assert_array_equal(fs.lookuptable['RGBA'][-1, :],
+                       array([ 221.,   60.,   60.,    0.]))
 
 
 def test_Freesurfer_04():
     region_label, approx = fs.find_brain_region([37, 48, 16])
-    assert region_label == 'ctx-rh-parsorbitalis'
+    assert region_label == 'ctx-rh-rostralmiddlefrontal'
     assert approx == 0
 
 
 def test_Freesurfer_05():
-    region_label, approx = fs.find_brain_region([0, 0, 0], 2)
-    assert region_label == '--not found--'
-    assert approx == 2
+    region_label, approx = fs.find_brain_region([10, 0, 0], max_approx=2)
+    assert region_label == 'Right-Cerebral-White-Matter'
+    assert approx == 0
 
 
 def test_Freesurfer_06():
-    region_label, approx = fs.find_brain_region([0, 0, 0], 5)
-    assert region_label == 'Left-VentralDC'
-    assert approx == 4
-
-
-def test_Freesurfer_07():
     l0, l1, l2 = fs.read_label('lh')
-    assert l0[-1] == 27
+    assert l0[-1] == 14
     assert l1.shape == (36, 5)
     assert l1[-1, -1] == 2146559
     assert l2[-1] == 'insula'
-"""
