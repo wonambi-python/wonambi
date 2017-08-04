7c724d61165d00e7c101b8569fdaa4f80028b78f
Gio on rgs <github@gpiantoni.com>
2017-08-04 09:23:59 -0400

    test for ktlx

---
+++ tests/test_phypno_ioeeg_ktlx.py
@@ -0,0 +1,21 @@
+from numpy.testing import assert_array_almost_equal
+
+from phypno import Dataset
+
+from .utils import IO_PATH
+
+xltek_file = IO_PATH / 'xltek'
+
+
+def test_xltek_data():
+    d = Dataset(xltek_file)
+    data = d.read_data(begsam=223380, endsam=223381, chan=('Fz', ))
+    assert_array_almost_equal(data.data[0][0, 0], -2021.171532)
+
+
+def test_xltek_marker():
+    d = Dataset(xltek_file)
+    markers = d.read_markers()
+    assert markers[0]['name'] == 'Gain/Filter change (-unknown-)'
+    assert markers[-1]['end'] == 1052.1
+
