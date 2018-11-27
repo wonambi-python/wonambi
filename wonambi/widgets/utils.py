50654be04771b97fcbfd8060e2faea3ff0e39512
Jordan O'Byrne <jnobyrne@users.noreply.github.com>
2018-11-27 16:51:45 -0500

    wonambi taskbar icon

--- wonambi/widgets/utils.py
+++ wonambi/widgets/utils.py
@@ -44,7 +44,7 @@ stdicon = QCommonStyle.standardIcon
 icon_path = join(dirname(realpath(__file__)), 'icons')
 oxy_path = join(icon_path, 'oxygen')
 
-ICON = {'application': join(icon_path, 'wonambi.jpg'),
+ICON = {'application': join(icon_path, 'wonambi.png'),
         'open_rec': join(oxy_path, 'document-open.png'),
         'page_prev': join(oxy_path, 'go-previous-view.png'),
         'page_next': join(oxy_path, 'go-next-view.png'),
